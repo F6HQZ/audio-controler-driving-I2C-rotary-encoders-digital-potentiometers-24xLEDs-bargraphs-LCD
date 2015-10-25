4a5ade81512e04e5b0299dd72a71b7b7957400f2
Francois <f6hqz-m@hamwlan.net>
2015-10-25 21:22:25 +0100

    header for lcd.c from wiringPI from Gordon Henderson

--- lcd.h
+++ lcd.h
@@ -45,7 +45,7 @@ extern void lcdPrintf      (const int fd, const char *message, ...) ;
 extern int  lcdInit (const int rows, const int cols, const int bits,
 	const int rs, const int strb,
 	const int d0, const int d1, const int d2, const int d3, const int d4,
-	const int d5, const int d6, const int d7) ;
+	const int d5, const int d6, const int d7) ; 
 
 #ifdef __cplusplus
 }
