c8bf7e6d60e5e2f560f9071bc3257796a58c705e
Francois <f6hqz-m@hamwlan.net>
2015-11-04 22:39:00 +0100

    add few GPIO extensions modules digital or analog

--- extensions-modules.c
+++ extensions-modules.c
@@ -60,7 +60,11 @@
 
 #include "extensions-modules.h"
 
-struct extension_module *setupModule(char *module_label, char *module_type, int module_address, int module_pinBase) ; 
+struct extension_module *setupModule(char *module_label, 
+	char *module_input_name_0, char *module_input_name_1, 
+	char *module_input_name_2, char *module_input_name_3, 
+	char *module_type, char *module_bus_type, int module_address, 
+	int module_pinBase) ; 
 	
 int numberofmodules = 0 ; // as writed, number of bargraphs, will be modified by the code later
 
@@ -69,7 +73,11 @@ int numberofmodules = 0 ; // as writed, number of bargraphs, will be modified by
 
 //======================================================================
 
-struct extension_module *setupModule(char *module_label, char *module_type, int module_address, int module_pinBase)
+struct extension_module *setupModule(char *module_label, 
+	char *module_input_name_0, char *module_input_name_1, 
+	char *module_input_name_2, char *module_input_name_3, 
+	char *module_type, char *module_bus_type, int module_address, 
+	int module_pinBase)
 {
 	if (numberofmodules > MAX_MODULES)
 	{
@@ -78,10 +86,17 @@ struct extension_module *setupModule(char *module_label, char *module_type, int 
 	}
 	
 	struct extension_module *newmodule = modules + numberofmodules++ ;
-	newmodule->module_label = module_label ;     // name or label as "Volume" or "Balance" or "Treble", etc...
-	newmodule->module_type = module_type ;       // type of chip, PCF8591, or of the described chips at top of the "extensions-modules.h" file
-	newmodule->module_address = module_address ; // address of the chip on the I2C or SPI bus (check with "gpio i2cdetect" Linux console instruction)
-	newmodule->module_pinBase = module_pinBase ; // first pin (base) address, need to be different for each converter and >64
+	newmodule->module_label = module_label ;         // name given to this chip, as "CONVERTER#1", as a reminder of "what is this $#% of chip for ?"
+	newmodule->module_type = module_type ;           // type of chip, PCF8591, or of the described chips at top of the "extensions-modules.h" file
+	newmodule->module_bus_type = module_bus_type ;   // 0 for I2C or 1 for SPI
+	newmodule->module_address = module_address ;     // address of the chip on the I2C or SPI bus (check with "gpio i2cdetect" Linux console instruction)
+	newmodule->module_pinBase = module_pinBase ;     // first pin (base) address, need to be different for each converter and >64
+	
+	
+	newmodule->module_input_name[0] = module_input_name_0 ; // name or label as "INPUT", "VOLUME" or "STAGE1" or "OUTPUT", etc... for each chip I/O channel
+	newmodule->module_input_name[1] = module_input_name_1 ; 
+	newmodule->module_input_name[2] = module_input_name_2 ; 
+	newmodule->module_input_name[3] = module_input_name_3 ; 
 	
 	if (module_type == "PCF8591")
 	{
